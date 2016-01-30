672d79424c4ad557efe9cbb859b26f9b3536d2a4
bkalb <bkalb@blanketfortgames.com>
2016-01-30 11:03:16 -0500

    Turned the runner back into a kinematic rigidbody Did a lot of fiddling with feel

--- Assets/Scripts/RunnerBehavior.cs
+++ Assets/Scripts/RunnerBehavior.cs
@@ -8,6 +8,99 @@ public class RunnerBehavior : MonoBehaviour
 {
 
     public float MaxJumpTime = 0.5f;
+    public float JumpPower = 350f;
+    public float Gravity = 300f;
+    public float BaseX = 1.37f;
+    public float XResetSpeed = 0.5f;
+    public bool Paused;
+
+    private bool _jumping = false;
+    private float _jumpTime = 0;
+    private Vector2 _velocity = Vector3.zero;
+    private bool _jumpKeyDown;
+    private bool _jumpKey;
+
+    private CharacterController2D _characterController2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        _characterController2D = GetComponent<CharacterController2D>();
+    }
+
+    void Update()
+    {
+        _jumpKeyDown = Input.GetKeyDown(KeyCode.W);
+        _jumpKey = Input.GetKey(KeyCode.W);
+
+        if (Paused)
+        {
+            return;
+        }
+
+        gameObject.transform.position += Vector3.left * 0.5f;
+
+        if (_characterController2D.isGrounded)
+        {
+            _velocity = Vector2.zero;
+        }
+        _velocity -= Gravity * Vector2.up * Time.deltaTime;
+
+        if (_jumpKeyDown && _characterController2D.isGrounded)
+        {
+            _jumping = true;
+            _jumpTime = 0;
+        }
+        if (_jumping)
+        {
+            _jumpTime += Time.fixedDeltaTime;
+            _velocity = Vector2.up * JumpPower;
+            if (_jumpTime > MaxJumpTime)
+            {
+                _jumping = false;
+            }
+        }
+
+        if (_characterController2D.collisionState.above)
+        {
+            _jumping = false;
+            _velocity.y = Mathf.Min(_velocity.y, 0);
+        }
+
+        if (!_jumpKey)
+        {
+            _jumping = false;
+            _velocity.y = Mathf.Min(_velocity.y, Mathf.Abs(_velocity.y / 2));
+        }
+
+        if (transform.position.x < BaseX && _characterController2D.isGrounded)
+        {
+            _velocity.x = XResetSpeed;
+        }
+        else
+        {
+            _velocity.x = 0;
+        }
+
+        GetComponent<CharacterController2D>().move(_velocity * Time.deltaTime - Vector2.left * 0.5f);
+
+        if (transform.position.y < -25)
+        {
+            transform.position += 30 * Vector3.up;
+        }
+    }
+}
+
+/*using System;
+using UnityEngine;
+using System.Collections;
+using System.Text;
+using Prime31;
+
+public class RunnerBehavior : MonoBehaviour
+{
+
+    public float MaxJumpTime = 0.5f;
     public float jetpackPower = 350f;
     public float jumpPower = 350f;
     public float Gravity = 300f;
@@ -96,4 +189,4 @@ public class RunnerBehavior : MonoBehaviour
         
         
     }
-}
+}*/
