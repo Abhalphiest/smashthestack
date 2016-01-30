61790e557c33151276bc3b5d537b09041769bc4d
Margaret Dorsey (RIT Student) <med7068@ad.rit.edu>
2016-01-29 20:29:07 -0500

    code that compiles

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -6,7 +6,7 @@ public class GameManagerScript : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-        simonList = new List<KeyCode>(); //for simon minigame
+        simonList = new List<int>(); //for simon minigame
 	}
 	
 	// Update is called once per frame
@@ -17,8 +17,9 @@ public class GameManagerScript : MonoBehaviour {
 
     #region Simon
 
+
     List<int> simonList; //currently implemented with ints, can be easily substituted for Unity Color
-    
+    Dictionary<int, KeyCode> keystrokeMap; //to get what keystroke is associated with each color
     /// <summary>
     /// simon runs the memory minigame
     /// </summary>
@@ -28,11 +29,26 @@ public class GameManagerScript : MonoBehaviour {
     {
         bool complete = false; //have they finished?
         float time = 0.0f; //the time elapsed
-        int listIndex; //index of what color we're on
+        int listIndex = 0; //index of what color we're on
         while(time < p_seconds)
         {
+           
+
+
             if(listIndex < simonList.Count)
             {
+                //display current color
+
+                //if correct key
+                if (Input.GetKeyDown(keystrokeMap[simonList[listIndex]]))
+                {
+                    listIndex++;
+                }
+                //else
+                else
+                {
+                    listIndex = 0;
+                }
 
             }
             else
