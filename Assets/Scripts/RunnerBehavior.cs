1937743fefdd567b277895347d0155791ec395c7
Margaret Dorsey (RIT Student) <med7068@ad.rit.edu>
2016-01-30 02:28:31 -0500

    main character implemented

--- Assets/Scripts/RunnerBehavior.cs
+++ Assets/Scripts/RunnerBehavior.cs
@@ -8,45 +8,42 @@ public class RunnerBehavior : MonoBehaviour
 {
 
     public float MaxJumpTime = 0.5f;
-    public float JumpPower = 6f;
-    public float Gravity = 5f;
+    public float JumpPower = 350f;
+    public float Gravity = 300f;
     public float BaseX = 0;
-    public float XResetSpeed = 0.5f;
+    public float XResetSpeed = 200f;
     public bool Paused;
-
+    public float runSpeed = .2f;
     private bool _jumping = false;
     private float _jumpTime = 0;
-    private Vector2 _velocity = Vector3.zero;
 
+    private Rigidbody2D rigidBody;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            _jumping = true;
+            _jumpTime = 0;
+        }
+    }
 	// Update is called once per frame
 	void FixedUpdate () {
+
 	    if (Paused)
 	    {
 	        return;
-	    }
-
-        gameObject.transform.position += Vector3.left * Time.fixedDeltaTime;
-
-        if (GetComponent<CharacterController2D>().isGrounded)
-	    {
-            _velocity = Vector2.zero;
-	    }
-        _velocity -= Gravity * Vector2.up * Time.fixedDeltaTime;
-
-        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<CharacterController2D>().isGrounded)
-	    {
-	        _jumping = true;
-	        _jumpTime = 0;
-	    }
+	    }        
         if(_jumping)
         {
+            
             _jumpTime += Time.fixedDeltaTime;
-            _velocity = Vector2.up*JumpPower;
+            rigidBody.AddForce(new Vector2(0.0f, JumpPower),ForceMode2D.Force);
+            
             if (_jumpTime > MaxJumpTime || !Input.GetKey(KeyCode.Space))
             {
                 _jumping = false;
@@ -55,13 +52,13 @@ public class RunnerBehavior : MonoBehaviour
 
 	    if (transform.position.x < BaseX)
 	    {
-	        _velocity.x = XResetSpeed;
+            rigidBody.AddForce(new Vector2(XResetSpeed, 0.0f), ForceMode2D.Force);
 	    }
         else
 	    {
-	        _velocity.x = 0;
+	        
 	    }
 
-        GetComponent<CharacterController2D>().move(_velocity*Time.fixedDeltaTime - Vector2.left * Time.fixedDeltaTime);
+        rigidBody.AddForce(new Vector2(0.0f, -Gravity), ForceMode2D.Force);
     }
 }
