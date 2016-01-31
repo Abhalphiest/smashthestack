f2a46c270d943c5ac9481a439535718e104b269e
Matthew Fasman (RIT Student) <mxf3998@ad.rit.edu>
2016-01-31 12:32:46 -0500

    Fixed game over menu glitch

--- Assets/Game Jam Menu Template/Scripts/toMainMenu.cs
+++ Assets/Game Jam Menu Template/Scripts/toMainMenu.cs
@@ -6,6 +6,7 @@ public class toMainMenu : MonoBehaviour {
     public void loadMainMenu()
     {
         Destroy(GameObject.FindGameObjectWithTag("UI"));
+        //Destroy(GameObject.FindGameObjectWithTag("UI"));
         Application.LoadLevel(0);
     }
 }
